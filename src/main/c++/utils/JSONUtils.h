30cfe9fa78d960fc2097d7d82ecbd006c10e6a51
atompacman <jeremiecoul@hotmail.com>
2015-03-28 15:46:34 -0400

    Add JSONUtils helper class

---
+++ src/main/c++/utils/JSONUtils.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "easylogging++.h"
+#include "rapidjson\document.h"
+#include "rapidjson\error\en.h"
+#include "..\engine\internal\dialog\FatalErrorDialog.h"
+
+using namespace rapidjson;
+
+static Value& parseJSON(Document&   o_Doc,
+                        const char* i_JSONFile,
+                        const char* i_RootElemName)
+{
+    // Open file
+    std::ifstream file(i_JSONFile);
+    if (file.bad()) {
+        std::stringstream ss;
+        ss << "Could not find JSON file \"" << i_JSONFile << "\"";
+        FatalErrorDialog(ss.str());
+    }
+
+    // Extract its content as a string
+    std::string content((std::istreambuf_iterator<char>(file)),
+        (std::istreambuf_iterator<char>()));
+
+    // Parse the JSON DOM
+    o_Doc.Parse(content.c_str());
+    ParseErrorCode code(o_Doc.GetParseError());
+    if (code != kParseErrorNone) {
+        std::stringstream ss;
+        ss << "File " << i_JSONFile << " is not a valid JSON file: "
+            << GetParseError_En(code) << " at " << o_Doc.GetErrorOffset();
+        FatalErrorDialog(ss.str());
+    }
+
+    // Check if it contains the root elem
+    if (!o_Doc.HasMember(i_RootElemName)) {
+        std::stringstream ss;
+        ss << "JSON file \"" << i_JSONFile << "\" root element is not \"" 
+            << i_RootElemName << "\"";
+        FatalErrorDialog(ss.str());
+    }
+
+    // Return document's root element
+    return o_Doc[i_RootElemName];
+}
+
+static const Value& getSubElem(const Value& i_Value, const char* i_Elem)
+{
+    if (!i_Value.HasMember(i_Elem)) {
+        std::stringstream ss;
+        ss << "Invalid map configuration file: Missing \"" <<
+            i_Elem << "\" element";
+        FatalErrorDialog(ss.str());
+    }
+    return i_Value[i_Elem];
+}
+
+static UINT getUINT(const Value& i_Value, const char* i_Elem)
+{
+    const Value& subElem(getSubElem(i_Value, i_Elem));
+    if (!subElem.IsUint()) {
+        std::stringstream ss;
+        ss << "Parameter \"" << i_Elem <<
+            "\" value should be a positive integer";
+        FatalErrorDialog(ss.str());
+    }
+    return subElem.GetUint();
+}
+
+static double getNormDouble(const Value& i_Value, const char* i_Elem)
+{
+    const Value& subElem(getSubElem(i_Value, i_Elem));
+    if (!subElem.IsDouble()) {
+        std::stringstream ss;
+        ss << "Parameter \"" << i_Elem <<
+            "\" value should be a real number";
+        FatalErrorDialog(ss.str());
+    }
+    double value(subElem.GetDouble());
+    if (value < 0 || value > 1.0) {
+        std::stringstream ss;
+        ss << "Parameter \"" << i_Elem <<
+            "\" value should be a real number between 0 and 1";
+        FatalErrorDialog(ss.str());
+    }
+    return value;
+}
