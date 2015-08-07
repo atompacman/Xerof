957046d22cae9f2774e54295d75358c8f21d1643
atompacman <jeremiecoul@hotmail.com>
2015-08-07 16:25:15 -0400

    Added default log file to avoir creating a "logs/myeasylog.log" file

--- src/Xerof.cpp
+++ src/Xerof.cpp
@@ -1,10 +1,10 @@
 //===========================================================================\\
 //  | =   =   =   =   =   =   =   =   LIB   =   =   =   =   =   =   =   =   = ||
+#define ELPP_DEFAULT_LOG_FILE "logs/xerof.log"
 #include "easylogging++.h"
 //  | =   =   =   =   =   =   =   =   SRC   =   =   =   =   =   =   =   =   = ||
 #include "engine\GameLoop.h"
 #include "civ\human\RangeOfSight.h"
-#include <iosfwd>
 /*============================================================================||
 | Main entry point of the game
 |-----------------------------------------------------------------------------||
@@ -18,44 +18,44 @@ void initGameEngine();
 
 int main(int argc, char** argv)
 {
-	// Initialize easylogging++ library
-	initLoggingLibrary();
+    // Initialize easylogging++ library
+    initLoggingLibrary();
 
-	// Welcome message !
-	LOG(INFO) <<"======-=====-====-===-==-=- XEROF -=-==-===-====-=====-======";
+    // Welcome message !
+    LOG(INFO) <<"======-=====-====-===-==-=- XEROF -=-==-===-====-=====-======";
 
-	// Initialize Allegro5 library
-	initGameEngine();
+    // Initialize Allegro5 library
+    initGameEngine();
 
     // Initialize random seed
     initRandSeed();
 
-	// Start the game loop
+    // Start the game loop
     GameLoop().startGame();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void initLoggingLibrary()
 {
-	el::Configurations conf;
-	if (!conf.parseFromFile(LOGGER_CONFIG_FILE))
-	{
-		LOG(WARNING) << "Easylogging++ configuration file "<< LOGGER_CONFIG_FILE 
+    el::Configurations conf;
+    if (!conf.parseFromFile(LOGGER_CONFIG_FILE))
+    {
+        LOG(WARNING) << "Easylogging++ configuration file " << LOGGER_CONFIG_FILE
             << " was not found. Using default parameters.";
-	}
-	el::Loggers::reconfigureAllLoggers(conf);
+    }
+    el::Loggers::reconfigureAllLoggers(conf);
 }
 
 void initGameEngine()
 {
-	if (!al_init()) {
-		FatalErrorDialog("Allegro 5 core library initialization failed.");
-	}
-	if (!al_init_image_addon()){
-		FatalErrorDialog("Allegro image add-on initialization failed.");
-	}
-	if (!al_init_primitives_addon()){
-		FatalErrorDialog("Allegro primitives add-on initialization failed.");
-	}
+    if (!al_init()) {
+        FatalErrorDialog("Allegro 5 core library initialization failed.");
+    }
+    if (!al_init_image_addon()){
+        FatalErrorDialog("Allegro image add-on initialization failed.");
+    }
+    if (!al_init_primitives_addon()){
+        FatalErrorDialog("Allegro primitives add-on initialization failed.");
+    }
 }
