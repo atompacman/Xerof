a986359d901027b29b620efba79213d1f7a9497d
atompacman <jeremie.coulombe@usherbrooke.ca>
2015-08-20 21:48:11 -0400

    Removed small functions in Xerof.cpp

--- src/Xerof.cpp
+++ src/Xerof.cpp
@@ -16,42 +16,20 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-void initLoggingLibrary();
-void initGameEngine();
-
 int main(int argc, char** argv)
 {
     // Initialize easylogging++ library
-    initLoggingLibrary();
-
-    // Welcome message !
-    LOG(INFO) <<"======-=====-====-===-==-=- XEROF -=-==-===-====-=====-======";
-
-    // Initialize Allegro5 library
-    initGameEngine();
-
-    // Initialize random seed
-    initRandSeed();
-
-    // Start the game loop
-    GameLoop().startGame();
-
-    return EXIT_SUCCESS;
-}
-
-void initLoggingLibrary()
-{
     el::Configurations conf;
-    if (!conf.parseFromFile(LOGGER_CONFIG_FILE))
-    {
-        LOG(WARNING) << "Easylogging++ configuration file " << LOGGER_CONFIG_FILE
+    if (!conf.parseFromFile(LOGGER_CONFIG_FILE)) {
+        LOG(WARNING) << "Easylogging++ configuration file "<< LOGGER_CONFIG_FILE
             << " was not found. Using default parameters.";
     }
     el::Loggers::reconfigureAllLoggers(conf);
-}
 
-void initGameEngine()
-{
+    // Welcome message
+    LOG(INFO) <<"======-=====-====-===-==-=- XEROF -=-==-===-====-=====-======";
+
+    // Initialize Allegro5 library
     if (!al_init()) {
         FatalErrorDialog("Allegro 5 core library initialization failed.");
     }
@@ -61,4 +39,12 @@ void initGameEngine()
     if (!al_init_primitives_addon()){
         FatalErrorDialog("Allegro primitives add-on initialization failed.");
     }
+
+    // Initialize random seed
+    initRandSeed();
+
+    // Start the game loop
+    GameLoop().startGame();
+
+    return EXIT_SUCCESS;
 }
